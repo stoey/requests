b8ad688527a55050dee680d4ccfd3d1ac93a9395
Dan Head <dan@stoey.net>
2011-10-14 23:18:09 +0000

    Monkeypatched urllib2.build_opener from Python 2.6

--- requests/monkeys.py
+++ requests/monkeys.py
@@ -7,9 +7,52 @@ requests.monkeys
 Urllib2 Monkey patches.
 
 """
-
+import httplib
 import urllib2
 import re
+
+
+def build_opener(*handlers):
+    """Create an opener object from a list of handlers.
+
+    The opener will use several default handlers, including support
+    for HTTP, FTP and when applicable, HTTPS.
+
+    If any of the handlers passed as arguments are subclasses of the
+    default handlers, the default handlers will not be used.
+    """
+    import types
+    def isclass(obj):
+        return isinstance(obj, types.ClassType) or hasattr(obj, "__bases__")
+
+    opener = urllib2.OpenerDirector()
+    default_classes = [urllib2.ProxyHandler, urllib2.UnknownHandler, urllib2.HTTPHandler,
+                       urllib2.HTTPDefaultErrorHandler, urllib2.HTTPRedirectHandler,
+                       urllib2.FTPHandler, urllib2.FileHandler, urllib2.HTTPErrorProcessor]
+    if hasattr(httplib, 'HTTPS'):
+        default_classes.append(urllib2.HTTPSHandler)
+    skip = set()
+    for klass in default_classes:
+        for check in handlers:
+            if isclass(check):
+                if issubclass(check, klass):
+                    skip.add(klass)
+            elif isinstance(check, klass):
+                skip.add(klass)
+    for klass in skip:
+        default_classes.remove(klass)
+
+    for klass in default_classes:
+        opener.add_handler(klass())
+
+    for h in handlers:
+        if isclass(h):
+            h = h()
+        opener.add_handler(h)
+    return opener
+
+urllib2.build_opener = build_opener
+
 
 class Request(urllib2.Request):
     """Hidden wrapper around the urllib2.Request object. Allows for manual
